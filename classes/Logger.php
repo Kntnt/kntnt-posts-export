b0f6947da4df71ef0aae6f6c603bb03b9a5139ac
Thomas Barregren <thomas@kntnt.se>
2020-10-26 20:26:27 +0100

    Added trace.

--- classes/Logger.php
+++ classes/Logger.php
@@ -1,32 +1,10 @@
 <?php
 
 
-namespace Kntnt\Posts_Export;
+namespace Kntnt\PostS_Export;
 
 
 trait Logger {
-
-    // If `$message` isn't a string, its value is printed. If `$message` is
-    // a string, it is written with each occurrence of '%s' replaced with
-    // the value of the corresponding additional argument converted to string.
-    // Any percent sign that should be written must be escaped with another
-    // percent sign, that is `%%`. This method do nothing if debug flag isn't
-    // set.
-    public static final function log( $message = '', ...$args ) {
-        if ( self::is_debugging() ) {
-            static::_log( $message, ...$args );
-        }
-    }
-
-    // If `$message` isn't a string, its value is printed. If `$message` is
-    // a string, it is written with each occurrence of '%s' replaced with
-    // the value of the corresponding additional argument converted to string.
-    // Any percent sign that should be written must be escaped with another
-    // percent sign, that is `%%`. This method works independent of
-    // the debug flag.
-    public static final function error( $message = '', ...$args ) {
-        static::_log( $message, ...$args );
-    }
 
     public static final function stringify( $val ) {
         if ( is_null( $val ) ) {
@@ -44,18 +22,65 @@ trait Logger {
         return $out;
     }
 
-    private static function _log( $message = '', ...$args ) {
+    public static function info( $message = '', ...$args ) {
+        return self::_log( 'INFO', $message, ...$args );
+    }
+
+    public static function error( $message = '', ...$args ) {
+        return self::_log( 'ERROR', $message, ...$args );
+    }
+
+    public static function debug( $message = '', ...$args ) {
+        if ( self::is_debugging() ) {
+            return self::_log( 'DEBUG', $message, ...$args );
+        }
+    }
+
+    public static function log( $context, $message = '', ...$args ) {
+        if ( in_array( strtoupper( $context ), [ 'INFO', 'ERROR' ] ) || self::is_debugging() ) {
+            return self::_log( $context, $message, ...$args );
+        }
+    }
+
+    public static final function trace( $message = '', ...$args ) {
         if ( ! is_string( $message ) ) {
             $args = [ $message ];
             $message = '%s';
         }
-        $caller = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 3 );
-        $caller = $caller[2]['class'] . '->' . $caller[2]['function'] . '()';
-        foreach ( $args as &$arg ) {
-            $arg = self::stringify( $arg );
+        $message = sprintf( $message, ...array_map( [ Plugin::class, 'stringify' ], $args ) );
+        $trace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS );
+        self::_echo( '[TRACE][#1]', $trace[1], $message );
+        for ( $i = 2; $i < count( $trace ); ++ $i ) {
+            self::_echo( "[TRACE][#{$i}]", $trace[ $i ] );
         }
-        $message = sprintf( $message, ...$args );
-        error_log( "$caller: $message" );
+    }
+
+    // If `$message` isn't a string, its value is printed. If `$message` is
+    // a string, it is written with each occurrence of '%s' replaced with
+    // the value of the corresponding additional argument converted to string.
+    // Any percent sign that should be written must be escaped with another
+    // percent sign, that is `%%`. The message is prefixed with [$context]
+    // followed by […] where … is the qualified name of the function calling.
+    protected static function _log( $context, $message, ...$args ) {
+        if ( ! is_string( $message ) ) {
+            $args = [ $message ];
+            $message = '%s';
+        }
+        $message = sprintf( $message, ...array_map( [ Plugin::class, 'stringify' ], $args ) );
+        $trace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 3 );
+        self::_echo( "[$context]", $trace[1], $message );
+        return [
+            'context' => $context,
+            'message' => $message,
+        ];
+    }
+
+    private static function _echo( $prefix, $step, $message = '' ) {
+        $caller = $step['function'];
+        if ( isset( $step['class'] ) ) {
+            $caller = $step['class'] . $step['type'] . $caller;
+        }
+        error_log( "{$prefix}[$caller]" . ( $message ? " $message" : '' ) );
     }
 
 }
